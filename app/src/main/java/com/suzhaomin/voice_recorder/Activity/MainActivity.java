53050cde5be14bdbf81340c9f20be7ecf0a7358f
suzhaomin666 <yfszm666@outlook.com>
2018-12-06 20:26:10 +0800

    加几个包

--- app/src/main/java/com/suzhaomin/voice_recorder/Activity/MainActivity.java
+++ app/src/main/java/com/suzhaomin/voice_recorder/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package com.suzhaomin.voice_recorder;
+package com.suzhaomin.voice_recorder.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(com.suzhaomin.voice_recorder.R.layout.activity_main);
         Log.v("改变","更改的地方");
     }
 }
